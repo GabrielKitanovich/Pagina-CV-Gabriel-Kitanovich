f6b118e1391b88368da625f919a0f47135b2927e
GabrielKitanovich <gabriel.kitanovich@gmail.com>
2025-08-01 17:34:18 -0300

    Add Hero Section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import './Hero.css';
+
+function Hero() {
+
+    return (
+        <>
+            <div className='Hero-container'>
+
+                <div className='Hero-content'>
+
+                    <div className='Hero-title Hero-text'>
+                        <h1>Hola, Soy Gabriel Kitanovich</h1>
+                    </div>
+                    
+                    <div className='Hero-subtitle Hero-text'>
+                        <p>Técnico en programación, desarrollador full-stack y entusiasta hacia la tecnología.</p>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    );
+    
+}
+export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 import '../styles/Home.css';
 
 
 
 function Home() {
     return (
-        <div className="Home"><Header />
-        <div>
-            Home
+        <div className="Home">
+            <Header />
+            <Hero />
+            <Footer />
         </div>
-        <Footer /></div>
     );
 };
 
